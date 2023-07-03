91944ca1a8c3f952085ff69f1c49e76122fbe78f
Jerome Ghartey <gharteyjerome@gmail.com>
2023-07-03 17:27:40 +0100

    finished colors tab

--- app/(dashboard)/[storeId]/(routes)/categories/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/categories/page.tsx
@@ -25,7 +25,7 @@ const CategoriesPage = async ({
         id: item.id,
         name: item.name,
         billboardLabel: item.billboard.label,
-        createdAt: format(item.createdAt, "MMMM do, yyy y")
+        createdAt: format(item.createdAt, "MMMM do, yyyy")
     }))
 
     return (
